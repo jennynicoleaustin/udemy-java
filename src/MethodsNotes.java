281f0a13448cbc28e1bfaffac180e610013e3f64
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-29 14:19:30 -0500

    through methods lecture notes

--- src/MethodsNotes.java
+++ src/MethodsNotes.java
@@ -1,2 +1,24 @@
 public class MethodsNotes {
+    public static void main(String[] args) {
+        // Methods to avoid repeating code blocks
+        calculateScore(true, 10000, 8, 200);
+        int highScore = calculateScore(2000, 200, 3, 2000); // you can assign a variable to the output of a method.
+    }
+
+    // New methods start outside the old method, but within the class
+    // public static - indicates a method
+    // void = do not return a value!
+    // replace void with the type of data you would like to have returned back to you
+
+    public static int calculateScore (boolean gameOver, int score, int levelCompleted, int bonus) {
+        //Once you have defined the parameter, java automatically defines the variable name, so you only need to assign a value to the variable.
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.format("Your final score was %s", finalScore);
+            return finalScore; // must specify a return value for both true and false outcomes
+        } return -1;
+    }
+
+    // DiffMerge Tool
 }
