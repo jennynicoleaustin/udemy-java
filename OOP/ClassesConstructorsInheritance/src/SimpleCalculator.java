7ff227e8e987a6842d5674b991aa0afb1fa5b5ce
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-10 15:22:49 -0500

    sum exercise

--- OOP/ClassesConstructorsInheritance/src/SimpleCalculator.java
+++ OOP/ClassesConstructorsInheritance/src/SimpleCalculator.java
@@ -1,5 +1,47 @@
-
+// Well done, your solution is correct!
 
 public class SimpleCalculator {
 
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber () {
+        return firstNumber;
+    }
+
+    public double getSecondNumber () {
+        return secondNumber;
+    }
+
+    public void setFirstNumber (double firstNumberField) {
+        firstNumber = firstNumberField;
+    }
+
+    public void setSecondNumber (double secondNumberField) {
+        secondNumber = secondNumberField;
+    }
+
+    public double getAdditionResult () {
+        double res = firstNumber + secondNumber;
+        return res;
+    }
+
+    public double getSubtractionResult () {
+        double res = firstNumber - secondNumber;
+        return res;
+    }
+
+    public double getMultiplicationResult () {
+        double res = firstNumber * secondNumber;
+        return res;
+    }
+
+    public double getDivisionResult () {
+        if (secondNumber != 0) {
+            double res = firstNumber / secondNumber;
+            return res;
+        } else {
+            return 0;
+        }
+    }
 }
