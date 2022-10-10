77a8c82f518c60b51c4a526dcb16cbdf64853dea
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-10 16:04:44 -0500

    returns correct values, with extra methods

--- OOP/ClassesConstructorsInheritance/src/Main.java
+++ OOP/ClassesConstructorsInheritance/src/Main.java
@@ -12,17 +12,27 @@ public class Main {
 //        System.out.println("divide= " + calculator.getDivisionResult());
 
 // Person Test
-        Person person = new Person();
-        person.setFirstName("");   // firstName is set to empty string
-        person.setLastName("");    // lastName is set to empty string
-        person.setAge(10);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setFirstName("John");    // firstName is set to John
-        person.setAge(18);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setLastName("Smith");    // lastName is set to Smith
-        System.out.println("fullName= " + person.getFullName());
+//        Person person = new Person();
+//        person.setFirstName("");   // firstName is set to empty string
+//        person.setLastName("");    // lastName is set to empty string
+//        person.setAge(10);
+//        System.out.println("fullName= " + person.getFullName());
+//        System.out.println("teen= " + person.isTeen());
+//        person.setFirstName("John");    // firstName is set to John
+//        person.setAge(18);
+//        System.out.println("fullName= " + person.getFullName());
+//        System.out.println("teen= " + person.isTeen());
+//        person.setLastName("Smith");    // lastName is set to Smith
+//        System.out.println("fullName= " + person.getFullName());
+
+// Cylinder
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
     }
 }
