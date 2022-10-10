ed6f17df9c6b651b6073d2631cd61270faaab22d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-10 16:07:45 -0500

    cylinder exercise correct

--- OOP/ClassesConstructorsInheritance/src/Cylinder.java
+++ OOP/ClassesConstructorsInheritance/src/Cylinder.java
@@ -1,22 +1,21 @@
+// Well done, your solution is correct!
+
 public class Cylinder extends Circle {
    private double height;
 
     public Cylinder(double radius, double height) {
         super(radius);
-        this.height = height;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
         if (height < 0) {
             this.height = 0;
         } else {
             this.height = height;
         }
     }
+
+    public double getHeight() {
+        return height;
+    }
+
 
     public double getVolume () {
         double area = getArea();
