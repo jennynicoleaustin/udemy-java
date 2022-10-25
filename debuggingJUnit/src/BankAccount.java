a1f28b2a7b4824af7a098203caafdd424f0dda47
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-25 09:35:09 -0500

    JUnit and debugging

--- debuggingJUnit/src/BankAccount.java
+++ debuggingJUnit/src/BankAccount.java
@@ -5,8 +5,14 @@ public class BankAccount {
     private String firstName;
     private String lastName;
     private double balance;
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 1;
+    private int accountType;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public boolean isChecking() {
+        return accountType == CHECKING;
+    }
+    public BankAccount(String firstName, String lastName, double balance, int accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
--- debuggingJUnit/src/BankAccountTest.java
+++ debuggingJUnit/src/BankAccountTest.java
@@ -4,29 +4,47 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        BankAccount account = new BankAccount("Jenny", "Austin", 1000.00);
+        BankAccount account = new BankAccount("Jenny", "Austin", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
         // the delta value is the margin of error allowed between expected and actual values.
-        assertEquals(1200.00, account.getBalance(), 0);
-        //every test should be able to run separately, and pass on its own without the need for running the other tests in the Test class
+
     }
-// best practices - you should run only one assert per test
+
+    // best practices - you should run only one assert per test
     @org.junit.jupiter.api.Test
     void withdraw() {
         fail("This test has not been implemented");
 
     }
 
+    // Name of the method should reflect the actual test being done.
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has not been implemented");
-
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Jenny", "Austin", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+        //every test should be able to run separately, and pass on its own without the need for running the other tests in the Test class
     }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Jenny", "Austin", 1000.00, BankAccount.CHECKING);
+        double balance = account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+        //every test should be able to run separately, and pass on its own without the need for running the other tests in the Test class
+    }
+
     @org.junit.jupiter.api.Test
     void dummyTest() {
         assertEquals(20, 20);
         //first value is what value you want to test and the second is the actual value.
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() {
+        BankAccount account = new BankAccount("Jenny", "Austin", 1000.00, BankAccount.CHECKING);
+        assertTrue(account.isChecking(), "The account is NOT a checking account");
     }
 }
 
